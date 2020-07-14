e88ffe6c44603f2d4a5be54ce510eac3e1c7da2d
Weissnix4711 <aldrian.thom@gmail.com>
2020-07-14 22:48:56 +0100

    Edit combat/index.js

--- scripts/combat/index.js
+++ scripts/combat/index.js
@@ -2,3 +2,4 @@ import "./hitSound.js" //Plays a sound on bow hit and thunder on kill
 import "./attackParticles.js" //Add restone blood particles when entity is hit
 import "./playerHealthBar.js" //Add a healthbar above mob head
 //import "./mobReward.js"
+import "./kits.js"
