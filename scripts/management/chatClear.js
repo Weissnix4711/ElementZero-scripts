69ae147b145444ad3e28db629d8015a89273d710
Weissnix4711 <aldrian.thom@gmail.com>
2020-07-15 00:07:41 +0100

    Edit management/chatClear.js

---
+++ scripts/management/chatClear.js
@@ -0,0 +1,31 @@
+import {
+    getPlayerByNAME
+} from "ez:player";
+
+import {
+    onChat
+} from "ez:chat";
+
+const system = server.registerSystem(0, 0);
+
+console.log("chatClear.js loaded");
+
+let clearHelp = ['.cc help'];
+let clear = ['.cc'];
+
+onChat((cmdObject) => {
+    try {
+        if (cmdObject.content === clearHelp[0]) {
+            system.executeCommand(`say Chat clearing commands are only for those that have the "staff" tag, and to clear the chat, type .cc`, () => {});
+        } else if (cmdObject.content === clear[0]) {
+            let player = getPlayerByNAME(cmdObject.sender);
+            let playerName = player.name;
+            for (i = 0; i < 30; i++) {
+                system.executeCommand(`execute @a[name="${playerName}",tag=staff] ~ ~ ~ say \n`, () => {};
+            }
+            system.executeCommand(`execute @a[name="$playerName}",tag=staff] ~ ~ ~ say §bChat Cleared!`, () => {});
+        }
+    } catch(err) {
+        console.error(err);
+    }
+});
