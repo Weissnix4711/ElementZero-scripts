cf28850e7ba626d243387335736de572bf060846
Weissnix4711 <aldrian.thom@gmail.com>
2020-07-16 20:32:27 +0100

    Beginnings of rewritten kits script

--- scripts/combat/kits.js
+++ scripts/combat/kits.js
@@ -1,3 +1,17 @@
+/*
+A kit system made by nickg two, it has 7 kits. 
+
+Features:
+- Optional only 1 kit per player
+- See which kits are available (.kit list)
+- Staff only kits
+- Fully customisable
+- Support for kit groups
+- Give the player any items you want, provided they are spawnable with commands. This includes addon items.
+
+For the staff kits, make sure you have the staff tag (/tag @s add staff)
+*/
+
 import {
     getPlayerByNAME
 } from "ez:player";
@@ -7,135 +21,57 @@ import {
 } from "ez:chat";
 
 import {
-	onPlayerInitialized
+    onPlayerInitialized
 } from "ez:player";
 
+import config from './kitsConfig';
+
 const system = server.registerSystem(0, 0);
+
 console.log("kits2.js loaded");
 
-//A kit system made by nickg two, it has 7 kits. With special features like only allowing 1 kit per player, to see what kits are availible, type .kit in chat. for staff wanting to use the staff kit, make sure you have the staff tag (/tag @s add staff)
-
-let test = ['.kit'];
-let kita = ['.kit archer'];
-let kit2 = ['.kit miner'];
-let kit3 = ['.kit lumberjack'];
-let kit4 = ['.kit warrior'];
-let kit5 = ['.kit mage'];
-let kit6 = ['.kit demolitionist'];
-let kit7 = ['.kit staff'];
+infoCommand = ['info', 'help'];
+getCommand = ['get', 'buy', 'redeem', 'purchase'];
 
 onChat((cmdObject) => {
     try {
-        if (cmdObject.content === test[0]) {
-			let player = getPlayerByNAME(cmdObject.sender);
-			let playerName = player.name;
-			system.executeCommand(`execute @a[name="${playerName}"] ~ ~ ~ tellraw @a {"rawtext":[{"text":"§bKits are: .kit archer, .kit miner, .kit lumberjack, .kit warrior, .kit mage, §a(.kit staff)§b and .kit demolitionist---kit script made by Nickg two and Cynical"}]}`, () => {});
-        }
-		if (cmdObject.content === kita[0]) {
-			 let player = getPlayerByNAME(cmdObject.sender);
-			let playerName = player.name;
-			system.executeCommand(`execute @a[name="${playerName}",scores={kit=0}] ~ ~ ~ give @s arrow 64 `, () => {});
-			system.executeCommand(`execute @a[name="${playerName}",scores={kit=0}] ~ ~ ~ give @s bow 1 `, () => {});
-			system.executeCommand(`execute @a[name="${playerName}",scores={kit=0}] ~ ~ ~ give @s cooked_beef 16 `, () => {});
-			system.executeCommand(`execute @a[name="${playerName}",scores={kit=0}] ~ ~ ~ give @s chainmail_helmet 1 `, () => {});
-			system.executeCommand(`execute @a[name="${playerName}",scores={kit=0}] ~ ~ ~ give @s chainmail_chestplate 1 `, () => {});
-			system.executeCommand(`execute @a[name="${playerName}",scores={kit=0}] ~ ~ ~ give @s chainmail_leggings 1 `, () => {});
-			system.executeCommand(`execute @a[name="${playerName}",scores={kit=0}] ~ ~ ~ give @s chainmail_boots 1 `, () => {});
-			system.executeCommand(`execute @a[name="${playerName}",scores={kit=0}] ~ ~ ~ say §b@s has chosen kit Archer! `, () => {});
-			system.executeCommand(`execute @a[name="${playerName}",scores={kit=1}] ~ ~ ~ say §b@s has already picked a kit! `, () => {});
-			system.executeCommand(`execute @a[name="${playerName}",scores={kit=0}] ~ ~ ~ scoreboard players set @s kit 1`,{});
-        }
-			if (cmdObject.content === kit2[0]) {
-				let player = getPlayerByNAME(cmdObject.sender);
-				let playerName = player.name;
-				system.executeCommand(`execute @a[name="${playerName}",scores={kit=0}] ~ ~ ~ give @s iron_pickaxe 1 `, () => {});
-				system.executeCommand(`execute @a[name="${playerName}",scores={kit=0}] ~ ~ ~ give @s iron_helmet 1 `, () => {});
-				system.executeCommand(`execute @a[name="${playerName}",scores={kit=0}] ~ ~ ~ give @s iron_chestplate 1 `, () => {});
-				system.executeCommand(`execute @a[name="${playerName}",scores={kit=0}] ~ ~ ~ give @s iron_leggings 1 `, () => {});
-				system.executeCommand(`execute @a[name="${playerName}",scores={kit=0}] ~ ~ ~ give @s iron_boots 1 `, () => {});
-				system.executeCommand(`execute @a[name="${playerName}",scores={kit=0}] ~ ~ ~ give @s stone_shovel 1 `, () => {});
-				system.executeCommand(`execute @a[name="${playerName}",scores={kit=0}] ~ ~ ~ give @s torch 32 `, () => {});
-				system.executeCommand(`execute @a[name="${playerName}",scores={kit=0}] ~ ~ ~ give @s cooked_beef 16 `, () => {});
-				system.executeCommand(`execute @a[name="${playerName}",scores={kit=0}] ~ ~ ~ say §b@s has chosen kit Miner! `, () => {});
-				system.executeCommand(`execute @a[name="${playerName}",scores={kit=1}] ~ ~ ~ say §b@s has already picked a kit! `, () => {});
-				system.executeCommand(`execute @a[name="${playerName}",scores={kit=0}] ~ ~ ~ scoreboard players set @s kit 1`,{});
-	}
-	if (cmdObject.content === kit3[0]) {
-		let player = getPlayerByNAME(cmdObject.sender);
-		let playerName = player.name;
-		system.executeCommand(`execute @a[name="${playerName}",scores={kit=0}] ~ ~ ~ give @s iron_axe 1 `, () => {});
-		system.executeCommand(`execute @a[name="${playerName}",scores={kit=0}] ~ ~ ~ give @s crafting_table 1 `, () => {});
-		system.executeCommand(`execute @a[name="${playerName}",scores={kit=0}] ~ ~ ~ give @s leather_chestplate 1 `, () => {});
-		system.executeCommand(`execute @a[name="${playerName}",scores={kit=0}] ~ ~ ~ give @s leather_leggings 1 `, () => {});
-		system.executeCommand(`execute @a[name="${playerName}",scores={kit=0}] ~ ~ ~ give @s leather_boots 1 `, () => {});
-		system.executeCommand(`execute @a[name="${playerName}",scores={kit=0}] ~ ~ ~ give @s wood 10 `, () => {});
-		system.executeCommand(`execute @a[name="${playerName}",scores={kit=0}] ~ ~ ~ give @s leather_helmet 32 `, () => {});
-		system.executeCommand(`execute @a[name="${playerName}",scores={kit=0}] ~ ~ ~ give @s cooked_beef 16 `, () => {});
-		system.executeCommand(`execute @a[name="${playerName}",scores={kit=0}] ~ ~ ~ say §b@s has chosen kit Lumberjack! `, () => {});
-		system.executeCommand(`execute @a[name="${playerName}",scores={kit=1}] ~ ~ ~ say §b@s has already picked a kit! `, () => {});
-		system.executeCommand(`execute @a[name="${playerName}",scores={kit=0}] ~ ~ ~ scoreboard players set @s kit 1`,{});
-	} 
-	if (cmdObject.content === kit4[0]) {
-		let player = getPlayerByNAME(cmdObject.sender);
-		let playerName = player.name;
-		system.executeCommand(`execute @a[name="${playerName}",scores={kit=0}] ~ ~ ~ give @s stone_sword 1 `, () => {});
-		system.executeCommand(`execute @a[name="${playerName}",scores={kit=0}] ~ ~ ~ give @s iron_helmet 1 `, () => {});
-		system.executeCommand(`execute @a[name="${playerName}",scores={kit=0}] ~ ~ ~ give @s iron_chestplate 1 `, () => {});
-		system.executeCommand(`execute @a[name="${playerName}",scores={kit=0}] ~ ~ ~ give @s iron_leggings 1 `, () => {});
-		system.executeCommand(`execute @a[name="${playerName}",scores={kit=0}] ~ ~ ~ give @s iron_boots 1 `, () => {});
-		system.executeCommand(`execute @a[name="${playerName}",scores={kit=0}] ~ ~ ~ give @s shield 1 `, () => {});
-		system.executeCommand(`execute @a[name="${playerName}",scores={kit=0}] ~ ~ ~ give @s cooked_beef 16 `, () => {});
-		system.executeCommand(`execute @a[name="${playerName}",scores={kit=0}] ~ ~ ~ say §b@s has chosen kit Warrior! `, () => {});
-		system.executeCommand(`execute @a[name="${playerName}",scores={kit=1}] ~ ~ ~ say §b@s has already picked a kit! `, () => {});
-		system.executeCommand(`execute @a[name="${playerName}",scores={kit=0}] ~ ~ ~ scoreboard players set @s kit 1`,{});
+        let player = getPlayerByNAME(cmdObject.sender);
+        let playerName = player.name;
 
-	}
-	if (cmdObject.content === kit5[0]) {
-		let player = getPlayerByNAME(cmdObject.sender);
-		let playerName = player.name;
-		system.executeCommand(`execute @a[name="${playerName}",scores={kit=0}] ~ ~ ~ give @s blaze_rod 3 `, () => {});
-		system.executeCommand(`execute @a[name="${playerName}",scores={kit=0}] ~ ~ ~ give @s enchanting_table 1 `, () => {});
-		system.executeCommand(`execute @a[name="${playerName}",scores={kit=0}] ~ ~ ~ give @s book 7 `, () => {});
-		system.executeCommand(`execute @a[name="${playerName}",scores={kit=0}] ~ ~ ~ give @s ender_pearl 5 `, () => {});
-		system.executeCommand(`execute @a[name="${playerName}",scores={kit=0}] ~ ~ ~ give @s brewing_stand 1 `, () => {});
-		system.executeCommand(`execute @a[name="${playerName}",scores={kit=0}] ~ ~ ~ give @s cooked_beef 16 `, () => {});
-		system.executeCommand(`execute @a[name="${playerName}",scores={kit=0}] ~ ~ ~ say §b@s has chosen kit Mage! `, () => {});
-		system.executeCommand(`execute @a[name="${playerName}",scores={kit=1}] ~ ~ ~ say §b@s has already picked a kit! `, () => {});
-		system.executeCommand(`execute @a[name="${playerName}",scores={kit=0}] ~ ~ ~ scoreboard players set @s kit 1`,{});
-	}
-	if (cmdObject.content === kit6[0]) {
-		let player = getPlayerByNAME(cmdObject.sender);
-		let playerName = player.name;
-		system.executeCommand(`execute @a[name="${playerName}",scores={kit=0}] ~ ~ ~ give @s tnt 5 `, () => {});
-		system.executeCommand(`execute @a[name="${playerName}",scores={kit=0}] ~ ~ ~ give @s flint_and_steel 1 `, () => {});
-		system.executeCommand(`execute @a[name="${playerName}",scores={kit=0}] ~ ~ ~ give @s fire_charge 7 `, () => {});
-		system.executeCommand(`execute @a[name="${playerName}",scores={kit=0}] ~ ~ ~ give @s iron_pickaxe 1 `, () => {});
-		system.executeCommand(`execute @a[name="${playerName}",scores={kit=0}] ~ ~ ~ give @s leather_chestplate 1 `, () => {});
-		system.executeCommand(`execute @a[name="${playerName}",scores={kit=0}] ~ ~ ~ give @s cooked_beef 16 `, () => {});
-		system.executeCommand(`execute @a[name="${playerName}",scores={kit=0}] ~ ~ ~ say §b@s has chosen kit Demolitionist! `, () => {});
-		system.executeCommand(`execute @a[name="${playerName}",scores={kit=1}] ~ ~ ~ say §b@s has already picked a kit! `, () => {});
-		system.executeCommand(`execute @a[name="${playerName}",scores={kit=0}] ~ ~ ~ scoreboard players set @s kit 1`,{});
-	}
-	if (cmdObject.content === kit7[0]) {
-		let player = getPlayerByNAME(cmdObject.sender);
-		let playerName = player.name;
-		system.executeCommand(`execute @a[name="${playerName}",tag=staff] ~ ~ ~ give @s command_block 1 `, () => {});
-		system.executeCommand(`execute @a[name="${playerName}",tag=staff] ~ ~ ~ give @s redstone block 1 `, () => {});
-		system.executeCommand(`execute @a[name="${playerName}",tag=staff] ~ ~ ~ gamemode c `, () => {});
-		system.executeCommand(`execute @a[name="${playerName}",tag=staff] ~ ~ ~ give @s diamond_sword 1 `, () => {});
-	}
-} catch(err) {
+        if (!cmdObject.content.startsWith(".kit") {return};
+        const command = cmdObject.content.search(/\.kit (.+) .*/);
+
+        //info command
+        if (infoCommand.includes(command)) {
+            system.executeCommand(`execute @a[name="${playerName}"] ~ ~ ~ tellraw @s {"rawtext":[{"text":"== Kits ==\nAvailable kits are: ${Object.keys(config)} script made by Nickg two and Cynical"}]}`, () => {});
+        }
+        
+        //redeem kit
+        else if (getCommand.includes(command)) {
+            const kitGroup = cmdObject.content.search(/\.kit .+ (.+) .+/);
+            const kitName = cmdObject.content.search(/\.kit .+ .+ (.+)/);
+
+            //list through every item and give it to player
+            for (i = 0; i < eval(`config.${kitGroup}.${kitName}.items.length`); i++) {
+                let itemName = eval(`config.${kitGroup}.${kitName}.items[i].name`);
+                let itemAmount = eval(`config.${kitGroup}.${kitName}.items[i].amount`);
+                system.executeCommand(`give "${playerName}" ${itemName} ${itemAmount}`, () => {});
+            }
+        }
+
+        else {
+            //invalid command
+            system.executeCommand(`execute @a[name="${playerName}"] ~ ~ ~ tellraw @s {"rawtext":[{"text":"Kits: error. Invalid subcommand. Run .kit info for more information."}]}`, () => {});
+        }
+
+    } catch(err) {
         console.error(err);
-	}
-	
-	
+    }
 });
 
 onPlayerInitialized(player => {
-	let playerName = player.name
-	system.executeCommand(`scoreboard objectives add kit dummy`,{});
-	system.executeCommand(`execute @a[name="${playerName}"] ~ ~ ~ scoreboard players add @s kit 0`,{});
-	system.executeCommand(`execute @a[name="${playerName}",scores={kit=0}] ~ ~ ~ title @s title Make sure to do .kit, n§b@s`,{});
-	
-	
+    let playerName = player.name
+    system.executeCommand(`scoreboard objectives add kit dummy`,{});
+    system.executeCommand(`execute @a[name="${playerName}"] ~ ~ ~ scoreboard players add @s kit 0`,{});
+    system.executeCommand(`execute @a[name="${playerName}",scores={kit=0}] ~ ~ ~ title @s title Make sure to do .kit, n§b@s`,{});
 })
--- scripts/combat/kitsConfig.js
+++ scripts/combat/kitsConfig.js
@@ -21,7 +21,7 @@ export default config = {
         "staffOnly": true,
         "kits": [
             "admin": {
-            }
+            },
             "troll": {
             }
         ]
