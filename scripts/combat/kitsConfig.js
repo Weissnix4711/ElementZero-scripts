8ade37b737b51932fdb0d4cb1be6cb08ee7134e4
Weissnix4711 <aldrian.thom@gmail.com>
2020-07-16 18:49:52 +0100

    Add propsed kits config file

---
+++ scripts/combat/kitsConfig.js
@@ -0,0 +1,29 @@
+export default config = {
+    "kitGroupA":{
+        "singleChoice": true,
+        "repurchase": false,
+        "staffOnly": false,
+        "kits": [
+            "kitNameA": {
+                "description": "blah blah",
+                "items": [
+                    { "name": "diamonds", "amount": 12 },
+                    { "name": "diamond_sword", "amount": 1 }
+                ]
+            },
+            "kitNameB": {
+            }
+        ]
+    },
+    "staff": {
+        "singleChoice": false,
+        "repurchase": true,
+        "staffOnly": true,
+        "kits": [
+            "admin": {
+            }
+            "troll": {
+            }
+        ]
+    }
+}
