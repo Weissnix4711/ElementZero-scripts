074239fbe3186b76de333e9df2066fb55905b2f2
Weissnix4711 <aldrian.thom@gmail.com>
2020-07-18 19:10:17 +0100

    Add more information to index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,8 @@
 /*
-This file is the entrypoint to all scripts.
-From here, you can import any other scripts you like.
+This file is the entrypoint to all scripts. From here, you can import any other scripts you like.
+Or, in this case, we are importing some simple scripts which themselves import other scripts.
+
+If you don't want to use a specifc script, just comment out the import command.
 */
 
 import "./combat/index.js"
