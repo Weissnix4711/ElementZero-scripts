ccb358d43cdfdf978a4d720c551f925ea90e599e
Weissnix4711 <aldrian.thom@gmail.com>
2020-07-21 12:12:54 +0100

    [wild] v1.1 - Add 120s timeout between runs

--- scripts/info/wild.js
+++ scripts/info/wild.js
@@ -1,3 +1,11 @@
+/*
+Adds .wild command to teleport you to a random location!
+
+Made by Nick and Weissnix4711
+
+v1.1
+*/
+
 import {
     getPlayerByNAME
 } from "ez:player";
@@ -11,16 +19,18 @@ console.log("wild.js loaded");
 
 let wild = ['.wild'];
 
-//a .wild command to teleport you to a random location! made by nickg two
-
 onChat((cmdObject) => {
     try {
 		if (cmdObject.content === wild[0]) {
 			let player = getPlayerByNAME(cmdObject.sender);
 			let playerName = player.name;
             //this below uses the spreadplayers command to teleport the player to a random location, you can change the numbers to change the places where it will teleport players
-            system.executeCommand(`execute @a[name="${playerName}"] ~ ~ ~ spreadplayers 10000 10000 300 500 @s`, () => {});
-            system.executeCommand(`execute @a[name="${playerName}"] ~ ~ ~ effect @s slow_falling 30 1 true`, () => {});
+            system.executeCommand(`execute @a[name="${playerName}",tag=wild] ~ ~ ~ spreadplayers 10000 10000 300 500 @s`, () => {});
+            system.executeCommand(`execute @a[name="${playerName}",tag=wild] ~ ~ ~ effect @s slow_falling 30 1 true`, () => {});
+            system.executeCommand(`execute @a[name="${playerName}",tag=wild] ~ ~ ~ tag @s remove wild`, () => {});
+            setTimeout(() => {
+                system.executeCommand(`execute @a[name="${playerName}"] ~ ~ ~ tag add @s wild`, () => {});
+            }, 120);
         }
     } catch(err) {
         console.error(err);
