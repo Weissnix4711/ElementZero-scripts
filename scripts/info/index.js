11c372be680f04d30f6e485976942cfe2e838f38
Weissnix4711 <aldrian.thom@gmail.com>
2020-07-23 15:31:59 +0100

    [scoreboard] Add to index.js

--- scripts/info/index.js
+++ scripts/info/index.js
@@ -3,3 +3,4 @@ console.log("=== Info ===");
 import "./chatCommands.js" //Chat commands
 import "./playerJoin.js" //Player join message / welcome text
 import "./wild.js" //Adds a .wild command
+import "./scoreboard.js" //Scoreboard
